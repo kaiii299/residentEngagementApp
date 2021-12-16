d615d5357d15d87815a502873f5290816c39f2e7
kaiii299 <Zhikai299@gmail.com>
2021-12-16 23:29:09 +0800

    template
    login, register, forgetpassword

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeleteAccountComponent } from './delete-account/delete-account.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { HomeComponent } from './home/home.component';
+import { OTPComponent } from './otp/otp.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent , pathMatch: "full"},
+{path:"register", component: RegisterComponent},
+{path:"forgetpassword", component: ForgetPasswordComponent},
+{path:"otp", component: OTPComponent},
+{path:"deleteaccount", component: DeleteAccountComponent},
+{path:"**", component: PageNotFoundComponent ,  pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import {MatSelectModule} from '@angular/material/select';
+import { MatOptionModule} from '@angular/material/core';
+import { OTPComponent } from './otp/otp.component';
+import { FormsModule  , ReactiveFormsModule  } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { DeleteAccountComponent } from './delete-account/delete-account.component';
+import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    PageNotFoundComponent,
+    NavBarComponent,
+    ForgetPasswordComponent,
+    OTPComponent,
+    DeleteAccountComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSelectModule,
+    MatOptionModule,
+    FormsModule,
+    MatStepperModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDialogModule,
+
+
   ],
+  entryComponents :[DeleteAccountComponent],
   providers: [],
   bootstrap: [AppComponent]
 })

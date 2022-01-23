e0d087a1fa7772b8f7ee559e9d7645d927d9c55f
HanAMTK <amtkhrc25@gmail.com>
2022-01-24 00:51:36 +0800

    added input survey dialog

--- src/app/resident-detail/resident-detail.component.ts
+++ src/app/resident-detail/resident-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit } from '@angular/core';
 import { ResidentService } from '../resident.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 
 @Component({
@@ -12,17 +14,43 @@ export class ResidentDetailComponent implements OnInit {
   residentId: string;
   residentDetail: any;
 
-  constructor(private residentService: ResidentService, private activatedRoute: ActivatedRoute) {
+  constructor(private residentService: ResidentService, private activatedRoute: ActivatedRoute, public dialog: MatDialog ) {
     this.residentId = this.activatedRoute.snapshot.queryParams.id;
     console.log(this.residentId)
    }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.residentService.getResidentById(this.residentId).subscribe(residentInfo =>{
       console.log(residentInfo);
       this.residentDetail = residentInfo;
     })
 
   }
+  openSurveyDialog(){
+    this.dialog.open(InputSurveyDialog,{
+      width: '700px',
+      height: '500px',
+      panelClass: 'custom-modalbox'
+    });
+  }
 
+}
+
+@Component({
+  selector: 'input-survey-dialog',
+  templateUrl: './inputSurvey-dialog.html',
+  styleUrls: ['./inputSurvey-dialog.scss']
+})
+export class InputSurveyDialog {
+  surveyForm: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+
+  }
+  ngOnInIt(): void {
+
+    this.surveyForm = this.formBuilder.group({
+
+    })
+
+  }
 }
